2ed01cd0bc4da34793274e741edf8cb1dc22a03d
arman-kapbasov <arman.kapbasov@hpe.com>
2016-04-20 11:50:39 -0700

    fixes for list all units

--- lib/topology_lib_systemctl/library.py
+++ lib/topology_lib_systemctl/library.py
@@ -178,13 +178,14 @@ def list_all_units(enode):
     :rtype: list
     :return: The list of all system units or None
     '''
-    cmd = ("systemctl list-units  -all")
+    cmd = ("systemctl list-units  -all | tail -n+2 | head -n -7")
     retval = enode(cmd, shell='bash')
     retval = retval.split('\n')
 
     ret_list = []
     for line in retval:
-        if "failed" or "not-found" not in line:
+        print(line)
+        if "failed" not in line or "not-found" not in line:
             ret_list.append(line[0])
         else:
             ret_list.append(line[1])
